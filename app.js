60021eacc026aaf7091816232516b7865cfc4dac
MARKRALA <mrala1124@gmail.com>
2022-10-12 08:44:09 +0800

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+const read = require('/read')
+const add = require('/add')
+
+//get users input
+const cmd = process.argv
+
+if(cmd[2]==="add") {//add
+//build the object
+const note ={
+    "id" cmd[3]
+    "title"
+}}
