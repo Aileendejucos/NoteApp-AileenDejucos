bd15951981b7b56b3ef6cceb56f79a8e75adfc58
aileen <dejucosaileen@gmail.com>
2022-10-12 09:00:51 +0800

    updated

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const read = require('/read')
-const add = require('/add')
+const read = require('./read')
+const add = require('./add')
 
 //get users input
 const cmd = process.argv
 
-if(cmd[2]==="add") {//add
+if(cmd[2]=== "add") {//add
 //build the object
 const note ={
-    "id" cmd[3]
-    "title"
-}}
+    "id":cmd[3],
+    "title":cmd[4],
+    "body":cmd[5]
+}
+//get note txt contents
+const oldNote = read()
+
+//add new note to not txt
+add (note,oldNote)
+}
+if (cmd[2] ==='read') {//read
+console.log(read())
+}
